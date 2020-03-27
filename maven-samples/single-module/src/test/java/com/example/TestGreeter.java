76d3ed3e63290b44c8dca25c86942fb3b872831b
Gejza Berecz <geberecz@gmail.com>
2020-03-27 14:43:33 +0100

    Simulate error

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessageNewYear() {
+  public void greetShouldIncludeGreetingMessageNew Year() {
     String someone = "World, Thanks you making me Happy New Year...";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
